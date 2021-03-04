fab99e67c003223b31a8a831d9669e7b0cca07d1
mhitchi <mayhitchings@gmail.com>
2021-03-04 16:14:17 -0500

    added all copy

---
+++ assets/js/countdown.js
@@ -0,0 +1,25 @@
+const countdownDate = new Date("April 17, 2021 00:00:00").getTime();
+
+//update the count down every 1 second
+let x = setInterval(() => {
+    //get todays date and time
+    let now = new Date().getTime();
+
+    //calc distance between now and count down date
+    let distance = countdownDate - now;
+
+    //time calc for days, hrs, min, secs
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //display result in the DOM
+    document.getElementById("countdown").innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds to go`;
+
+    //if countdown expired
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "IT'S ON!"
+    }
+}, 1000)
