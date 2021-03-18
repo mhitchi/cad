8909041b2a2e5b0fa9b3c0bd442e4eb7fb48aafe
mhitchi <mayhitchings@gmail.com>
2021-03-18 15:22:40 -0400

    adding commas correctly

--- assets/js/data.js
+++ assets/js/data.js
@@ -13,17 +13,14 @@ const getData = () => {
             if (totalArr.length > 3){
                 //comma count = number of times arr length divisible by 3
                 let commaCount = totalArr.length/3;
-                //for the number of commas
-                // for (let i = 0; i<commaCount; i++) {
-                    let j;
-                    //if there are no commas, add one 3 places from end
-                    if (totalArr.indexOf(",") > -1){
-                        j = totalArr.length - 2;
-                    } else {
-                        j = totalArr.indexOf(",") - 2;
-                    }
+                let lastComma = totalArr.length - 3;
+                //place last comma
+                totalArr.splice(lastComma, 0, ",");
+                //place rest of commas
+                while (totalArr.indexOf(",") > 3){
+                    let j = totalArr.indexOf(",") - 3;
                     totalArr.splice(j, 0, ",");
-                // }
+                }
             }
             totalStr = totalArr.join("");
             document.getElementById("totalGiftAmt").innerHTML = "$" + totalStr + " Raised";
