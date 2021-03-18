f911aaeaf457962016fa3bd20ea338706e1c20c7
mhitchi <mayhitchings@gmail.com>
2021-03-18 15:12:47 -0400

    troubleshooting commas

--- assets/js/data.js
+++ assets/js/data.js
@@ -6,7 +6,27 @@ const getData = () => {
         }).then((response) => {
             //console.log(response);
             let total = parseInt(response.TotalGiftAmount);
-            document.getElementById("totalGiftAmt").innerHTML = "$" + total + " Raised";
+            //make arr of total
+            let totalStr = total.toString();
+            let totalArr = totalStr.split("");
+            //add ,'s
+            if (totalArr.length > 3){
+                //comma count = number of times arr length divisible by 3
+                let commaCount = totalArr.length/3;
+                //for the number of commas
+                // for (let i = 0; i<commaCount; i++) {
+                    let j;
+                    //if there are no commas, add one 3 places from end
+                    if (totalArr.indexOf(",") > -1){
+                        j = totalArr.length - 2;
+                    } else {
+                        j = totalArr.indexOf(",") - 2;
+                    }
+                    totalArr.splice(j, 0, ",");
+                // }
+            }
+            totalStr = totalArr.join("");
+            document.getElementById("totalGiftAmt").innerHTML = "$" + totalStr + " Raised";
         })
 }
 
