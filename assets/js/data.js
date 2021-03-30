9471dd4a3201014dd68edf560a9589b386bbd122
mhitchi <mayhitchings@gmail.com>
2021-03-30 10:01:40 -0400

    added fb preview images and meta description

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,90 +1,120 @@
-let numberStr;
+$(document).ready(function(){
+    let numberStr;
 
-const addCommas = number => {
-    //make arr of total
-    numberStr = number.toString();
-    let numberArr = numberStr.split("");
-    //add ,'s
-    if (numberArr.length > 3){
-        //comma count = number of times arr length divisible by 3
-        let commaCount = numberArr.length/3;
-        let lastComma = numberArr.length - 3;
-        //place last comma
-        numberArr.splice(lastComma, 0, ",");
-        //place rest of commas
-        while (numberArr.indexOf(",") > 3){
-            let j = numberArr.indexOf(",") - 3;
-            numberArr.splice(j, 0, ",");
+    const doCount = (element) => {
+        // COUNTER
+        let start // set on the first step to the timestamp provided
+
+        const finalNum = parseInt(element.innerHTML, 10) // parse out the final number
+        //console.log(finalNum);
+        const duration = 1000 // duration in ms
+        const step = ts => {
+            if (!start) {
+                start = ts
+            }
+            // get the time passed as a fraction of total duration
+            let progress = (ts - start) / duration 
+
+            element.textContent = Math.floor(progress * finalNum) // set the text
+            if (progress < 1) {
+                // if we're not 100% complete, request another animation frame
+                requestAnimationFrame(step) 
+                // console.log(finalNum)
+            } else {
+                el.textContent = finalNum;
+                // console.log(finalNum)
+            }
         }
+        // start the animation
+        requestAnimationFrame(step);
     }
-    numberStr = numberArr.join("");
-    return numberStr;
-}
 
-const noNeg = (number) => {
-    let amtLeft;
-    if (35000 - number >= 0) {
-        amtLeft = 35000 - number;
-    } else {
-        amtLeft = 0;
+    const addCommas = number => {
+        //make arr of total
+        numberStr = number.toString();
+        let numberArr = numberStr.split("");
+        //add ,'s
+        if (numberArr.length > 3){
+            //comma count = number of times arr length divisible by 3
+            let commaCount = numberArr.length/3;
+            let lastComma = numberArr.length - 3;
+            //place last comma
+            numberArr.splice(lastComma, 0, ",");
+            //place rest of commas
+            while (numberArr.indexOf(",") > 3){
+                let j = numberArr.indexOf(",") - 3;
+                numberArr.splice(j, 0, ",");
+            }
+        }
+        numberStr = numberArr.join("");
+        return numberStr;
     }
-    return amtLeft;
-}
 
-const percent = (goal, number) => {
-    let perc = (number/goal) * 100;
-    return parseInt(perc);
-}
+    const noNeg = (number) => {
+        let amtLeft;
+        if (35000 - number >= 0) {
+            amtLeft = 35000 - number;
+        } else {
+            amtLeft = 0;
+        }
+        return amtLeft;
+    }
 
-const getData = () => {
-    $.ajax({
-            url: "https://dev.dar.uga.edu/alumni/cad/test.json",
-            dataType: 'json',
-            type: 'GET',
-        }).then((response) => {
-            //console.log(response);
-            let total = parseInt(response.TotalGiftAmount);
-            let newDonorAmountLeft = noNeg(parseInt(response.NewDonorCappedGiftAmount));
-            let youngDonorAmountLeft = noNeg(parseInt(response.GOLDCappedGiftAmount))
+    const percent = (goal, number) => {
+        let perc = (number/goal) * 100;
+        return parseInt(perc);
+    }
 
-            let newDonorPercent = percent(35000, parseInt(response.NewDonorCappedGiftAmount));
-            let youngDonorPercent = percent(35000, parseInt(response.GOLDCappedGiftAmount));
-            let deiDonorPercent = percent(250, parseInt(response.DEIDonorCnt));
+    const getData = () => {
+        $.ajax({
+                url: "https://dev.dar.uga.edu/alumni/cad/test.json",
+                dataType: 'json',
+                type: 'GET',
+            }).then((response) => {
+                //console.log(response);
+                let total = parseInt(response.TotalGiftAmount);
+                let newDonorAmountLeft = noNeg(parseInt(response.NewDonorCappedGiftAmount));
+                let youngDonorAmountLeft = noNeg(parseInt(response.GOLDCappedGiftAmount))
 
-            //add commas
-            addCommas(total);
-            addCommas(newDonorAmountLeft);
+                let newDonorPercent = percent(35000, parseInt(response.NewDonorCappedGiftAmount));
+                let youngDonorPercent = percent(35000, parseInt(response.GOLDCappedGiftAmount));
+                let deiDonorPercent = percent(250, parseInt(response.DEIDonorCnt));
 
-            //print to DOM
-            document.getElementById("totalGiftAmt").innerHTML = "$" + addCommas(total);
-            document.getElementById("newDonorAmtLeft").innerHTML = "$" + addCommas(newDonorAmountLeft);
-            document.getElementById("youngDonorAmtLeft").innerHTML = "$" + addCommas(youngDonorAmountLeft);
-            document.getElementById("newDonorCnt").innerHTML = response.NewDonorCnt;
-            document.getElementById("youngDonorCnt").innerHTML = response.GOLDDonorCnt;
-            document.getElementById("deiDonorCnt").innerHTML = response.DEIDonorCnt;
-            
-            document.getElementById("newDonorPercent").innerHTML = newDonorPercent + "%";
-            document.getElementById("progress01").style.width = newDonorPercent + "%";
-            document.getElementById("youngDonorPercent").innerHTML = youngDonorPercent + "%";
-            document.getElementById("progress02").style.width = youngDonorPercent + "%";
-            document.getElementById("progress03").style.width = deiDonorPercent + "%";
-            document.getElementById("deiTotalGiftAmt").innerHTML = "$" + addCommas(parseInt(response.DEITotalGiftAmount));
-            
-        })
-}
+                //add commas
+                addCommas(total);
+                addCommas(newDonorAmountLeft);
 
-getData();
+                //print to DOM
+                document.getElementById("totalGiftAmt").innerHTML = "$" + addCommas(total);
+                document.getElementById("newDonorAmtLeft").innerHTML = "$" + addCommas(newDonorAmountLeft);
+                document.getElementById("youngDonorAmtLeft").innerHTML = "$" + addCommas(youngDonorAmountLeft);
+                document.getElementById("newDonorCnt").innerHTML = response.NewDonorCnt;
+                document.getElementById("youngDonorCnt").innerHTML = response.GOLDDonorCnt;
+                document.getElementById("deiDonorCnt").innerHTML = response.DEIDonorCnt;
+                
+                document.getElementById("newDonorPercent").innerHTML = newDonorPercent + "%";
+                document.getElementById("progress01").style.width = newDonorPercent + "%";
+                document.getElementById("youngDonorPercent").innerHTML = youngDonorPercent + "%";
+                document.getElementById("progress02").style.width = youngDonorPercent + "%";
+                document.getElementById("progress03").style.width = deiDonorPercent + "%";
+                document.getElementById("deiTotalGiftAmt").innerHTML = "$" + addCommas(parseInt(response.DEITotalGiftAmount));
+                
+            })
+    }
 
-//totalGiftAmt
+    getData();
 
-// newDonorAmtLeft
-// newDonorPercent
-// newDonorCnt
+    //totalGiftAmt
 
-// youngDonorAmtLeft
-// youngDonorPercent
-// youngDonorCnt
+    // newDonorAmtLeft
+    // newDonorPercent
+    // newDonorCnt
 
-// deiGoal
-// deiDonorCnt
-// deiTotalGiftAmt
+    // youngDonorAmtLeft
+    // youngDonorPercent
+    // youngDonorCnt
+
+    // deiGoal
+    // deiDonorCnt
+    // deiTotalGiftAmt
+})
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,139 +1,141 @@
-var portrait = window.matchMedia("(max-width: 600px)");
-var landscape = window.matchMedia("(min-width: 601px)");
+$(document).ready(function(){
+  var portrait = window.matchMedia("(max-width: 600px)");
+  var landscape = window.matchMedia("(min-width: 601px)");
 
-// When the user scrolls down 100px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
+  // When the user scrolls down 100px from the top of the document, resize the header's font size
+  window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("header").style.height = "100px";
-    if (portrait.matches) {
-      // document.getElementById("logo").style.margin = "0 45px !important";
-      document.getElementById("logo").style.height = "120px";
-      // margin top not working
-      document.getElementById("EDC").style.marginTop = "2rem !important";
-    } else if (landscape.matches) {
-      document.getElementById("logo").style.margin = "0 0 !important";
-      document.getElementById("logo").style.height = "120px";
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      document.getElementById("header").style.height = "100px";
+      if (portrait.matches) {
+        // document.getElementById("logo").style.margin = "0 45px !important";
+        document.getElementById("logo").style.height = "120px";
+        // margin top not working
+        document.getElementById("EDC").style.marginTop = "2rem !important";
+      } else if (landscape.matches) {
+        document.getElementById("logo").style.margin = "0 0 !important";
+        document.getElementById("logo").style.height = "120px";
+      } else {
+        document.getElementById("logo").style.height = "120px";
+      }
+      document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.75)";
     } else {
-      document.getElementById("logo").style.height = "120px";
+      document.getElementById("header").style.height = "160px";
+      if (portrait.matches) {
+        document.getElementById("logo").style.margin = "0 0";
+        document.getElementById("logo").style.height = "150px";
+        document.getElementById("totalGiftAmt").style.display = "block";
+      } else if (landscape.matches) {
+        document.getElementById("logo").style.margin = "0 0 !important";
+        document.getElementById("logo").style.height = "160px";
+      } else {
+        document.getElementById("logo").style.height = "220px";
+      }
+      document.getElementById("header").style.backgroundColor = "rgba(0,0,0,1)";
+      
     }
-    document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0.75)";
-  } else {
-    document.getElementById("header").style.height = "160px";
-    if (portrait.matches) {
-      document.getElementById("logo").style.margin = "0 0";
-      document.getElementById("logo").style.height = "150px";
-      document.getElementById("totalGiftAmt").style.display = "block";
-    } else if (landscape.matches) {
-      document.getElementById("logo").style.margin = "0 0 !important";
-      document.getElementById("logo").style.height = "160px";
-    } else {
-      document.getElementById("logo").style.height = "220px";
-    }
-    document.getElementById("header").style.backgroundColor = "rgba(0,0,0,1)";
     
   }
-  
-}
 
 
-// scrollFunction(mobile); // Call listener function at run time
-// mobile.addListener(scrollFunction); // Attach listener function on state changes
+  // scrollFunction(mobile); // Call listener function at run time
+  // mobile.addListener(scrollFunction); // Attach listener function on state changes
 
-// scrollFunction(landscape); // Call listener function at run time
-// landscape.addListener(scrollFunction); // Attach listener function on state changes
+  // scrollFunction(landscape); // Call listener function at run time
+  // landscape.addListener(scrollFunction); // Attach listener function on state changes
 
-const show = () => {
-  document.getElementById("vid").src = "assets/images/placeholder-vid-bg03.jpg";
-}
+  const show = () => {
+    document.getElementById("vid").src = "assets/images/placeholder-vid-bg03.jpg";
+  }
 
-const hide = () => {
-  document.getElementById("vid").src = "assets/images/placeholder-vid-bg02.jpg";
-}
+  const hide = () => {
+    document.getElementById("vid").src = "assets/images/placeholder-vid-bg02.jpg";
+  }
 
 
-CountDownTimer('04/17/2021 10:1 AM', 'countdown');
-CountDownTimer('04/24/2021 10:1 AM', 'newcountdown');
+  CountDownTimer('04/17/2021 10:1 AM', 'countdown');
+  CountDownTimer('04/24/2021 10:1 AM', 'newcountdown');
 
-function CountDownTimer(dt, id)
-{
-    var end = new Date(dt);
+  function CountDownTimer(dt, id)
+  {
+      var end = new Date(dt);
 
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour = _minute * 60;
-    var _day = _hour * 24;
-    var timer;
+      var _second = 1000;
+      var _minute = _second * 60;
+      var _hour = _minute * 60;
+      var _day = _hour * 24;
+      var timer;
 
-    function showRemaining() {
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
+      function showRemaining() {
+          var now = new Date();
+          var distance = end - now;
+          if (distance < 0) {
 
-            clearInterval(timer);
-            document.getElementById(id).innerHTML = '0';
+              clearInterval(timer);
+              document.getElementById(id).innerHTML = '0';
 
-            return;
-        }
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
+              return;
+          }
+          var days = Math.floor(distance / _day);
+          var hours = Math.floor((distance % _day) / _hour);
+          var minutes = Math.floor((distance % _hour) / _minute);
+          var seconds = Math.floor((distance % _minute) / _second);
 
-        // document.getElementById(id).innerHTML = days /*+ ' days'*/;
-        //document.getElementById(id).innerHTML += hours + 'hrs ';
-        //document.getElementById(id).innerHTML += minutes + 'mins ';
-        //document.getElementById(id).innerHTML += seconds + 'secs';
-    }
-
-    timer = setInterval(showRemaining, 10000);
-}
-
-//tab functionality
-//get array of .nav-link
-//forEach, on-click, make .tab-pane class active
-////remove active from other 2
-const navLinkArr = Array.from(document.getElementsByClassName("nav-link"));
-const tabArr = Array.from(document.getElementsByClassName("tab-pane"));
-
-navLinkArr.forEach(link => {
-  link.addEventListener("click", function () {
-    // if tab id fits in nav-link id
-    // add "active" to tab class and nav-link class
-    tabArr.forEach(tab => {
-      if(link.id.indexOf(tab.id) > -1){
-        tab.classList.add("show");
-        tab.classList.add("active");
-        link.classList.add("active");
-      } else if(link.id.indexOf(tab.id) <= -1) {
-        tab.classList.remove("show");
-        tab.classList.remove("active");
-        link.classList.remove("active");
+          // document.getElementById(id).innerHTML = days /*+ ' days'*/;
+          //document.getElementById(id).innerHTML += hours + 'hrs ';
+          //document.getElementById(id).innerHTML += minutes + 'mins ';
+          //document.getElementById(id).innerHTML += seconds + 'secs';
       }
+
+      timer = setInterval(showRemaining, 10000);
+  }
+
+  //tab functionality
+  //get array of .nav-link
+  //forEach, on-click, make .tab-pane class active
+  ////remove active from other 2
+  const navLinkArr = Array.from(document.getElementsByClassName("nav-link"));
+  const tabArr = Array.from(document.getElementsByClassName("tab-pane"));
+
+  navLinkArr.forEach(link => {
+    link.addEventListener("click", function () {
+      // if tab id fits in nav-link id
+      // add "active" to tab class and nav-link class
+      tabArr.forEach(tab => {
+        if(link.id.indexOf(tab.id) > -1){
+          tab.classList.add("show");
+          tab.classList.add("active");
+          link.classList.add("active");
+        } else if(link.id.indexOf(tab.id) <= -1) {
+          tab.classList.remove("show");
+          tab.classList.remove("active");
+          link.classList.remove("active");
+        }
+      })
     })
   })
-})
 
-//pulsing sticker show incentive on mouseover
-const stickerArr = Array.from(document.getElementsByClassName("sticker"));
-const incentiveArr = Array.from(document.getElementsByClassName("incentive"));
+  //pulsing sticker show incentive on mouseover
+  const stickerArr = Array.from(document.getElementsByClassName("sticker"));
+  const incentiveArr = Array.from(document.getElementsByClassName("incentive"));
 
-stickerArr.forEach(sticker => { 
-  sticker.addEventListener("mouseenter", function() { 
-    incentiveArr.forEach(incentive => {
-    incentive.classList.remove("hidden");
-    incentive.classList.add("animate__fadeIn");
-    incentive.classList.remove("animate__fadeOut");
-    sticker.classList.remove("animate__pulse");
-    }); 
-  });
-  sticker.addEventListener("mouseout", function() { 
-    incentiveArr.forEach(incentive => {
-    incentive.classList.add("animate__fadeOut");
-    incentive.classList.add("hidden");
-    incentive.classList.remove("animate__fadeIn");
-    sticker.classList.add("animate__pulse");
-    }); 
-  });
+  stickerArr.forEach(sticker => { 
+    sticker.addEventListener("mouseenter", function() { 
+      incentiveArr.forEach(incentive => {
+      incentive.classList.remove("hidden");
+      incentive.classList.add("animate__fadeIn");
+      incentive.classList.remove("animate__fadeOut");
+      sticker.classList.remove("animate__pulse");
+      }); 
+    });
+    sticker.addEventListener("mouseout", function() { 
+      incentiveArr.forEach(incentive => {
+      incentive.classList.add("animate__fadeOut");
+      incentive.classList.add("hidden");
+      incentive.classList.remove("animate__fadeIn");
+      sticker.classList.add("animate__pulse");
+      }); 
+    });
+  })
 })
